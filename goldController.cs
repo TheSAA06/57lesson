d02ec80a5bcb0427b65438c231e1c329fddb9522
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-02 09:52:52 +0900

    Add files via upload

---
+++ goldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goldController : MonoBehaviour
+{
+    AudioSource audio;
+    MeshRenderer display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        display = GetComponent<MeshRenderer>();
+        audio.Stop();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        audio.Play();
+        display.enabled = false;
+    }
+}
---
+++ noGoldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noGoldController : MonoBehaviour
+{
+    AudioSource audio;
+    MeshRenderer display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        display = GetComponent<MeshRenderer>();
+        audio.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        audio.Play();
+        display.enabled = false;
+    }
+}
